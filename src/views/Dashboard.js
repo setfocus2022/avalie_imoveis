4d45ab8218afe8f525d0f8492d3c1c5bf0cbc500
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-25 07:54:44 -0300

    home office

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -50,7 +50,7 @@ const Dashboard = () => {
             title="OS"
             width="900"
             height="550"
-            src="https://docs.google.com/spreadsheets/d/1A__5WacsvBT4hsyCHvaeg4RkWbu4sp55hIa3oFyB8uY/edit"
+            src="https://app.powerbi.com/view?r=eyJrIjoiNDg2NzAxYzUtMzhhOC00OWQ5LWE4ZDUtOTRlNTA0YmFlYWE4IiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9"
             frameBorder="0"
             allowFullScreen
           ></iframe>
