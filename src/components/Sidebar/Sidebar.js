312f0a73894d33c81ebbc2c3c8e673b4caba60a7
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-31 08:19:51 -0300

    checklist

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -23,7 +23,7 @@ function Sidebar({ color, image, routes }) {
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="https://www.qualitysegconsultoria.com.br"
             className="simple-text logo-mini mx-1"
           >
             <div>
