5101e8c7ff3413c2e1a0344dff9a8636b246995c
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-27 10:05:32 -0300

    base de conhecimento

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -18,10 +18,11 @@ const BaseConhecimento = () => {
         </Row>
         <Row>
           <Col>
-          <p><center>TESTE 123</center></p>
+          <p><center>Use @menções em seus E-mails #qsdicas
+</center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container">
-              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+              <center><iframe width="560" height="315" src="https://www.youtube.com/watch?v=zgv2hH-2rHo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
               </div>
             </BootstrapCard>
           </Col>
