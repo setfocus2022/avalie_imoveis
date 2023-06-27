dad3f44838585c57cad55a16313961aa81faf8cf
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-27 15:35:36 -0300

    Bacon

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -8,7 +8,8 @@ const BaseConhecimento = () => {
       <Container fluid>        
         <Row className="justify-content-center">
           <Col md={10}>
-          <p><center>TESTE 123</center></p>
+          <p><center>Video dos Cursos EAD para compartilhar</center></p>
+          <p><center>      </center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container" style={{ position: 'relative', overflow: 'hidden', paddingTop: '56.25%' }}>
                 <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
