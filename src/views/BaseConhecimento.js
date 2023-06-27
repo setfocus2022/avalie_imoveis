02b70691c714c44b24690ba4c47847114d515699
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-27 12:29:44 -0300

    rapaiz

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -19,10 +19,10 @@ const BaseConhecimento = () => {
 
         <Row className="justify-content-center">
           <Col md={10}>
-          <p><center>TESTE 123</center></p>
+          <p><center>Use @menções em seus E-mails #qsdicas</center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container" style={{ position: 'relative', overflow: 'hidden', paddingTop: '56.25%' }}>
-                <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/zgv2hH-2rHo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
             </BootstrapCard>
           </Col>
@@ -30,10 +30,10 @@ const BaseConhecimento = () => {
 
         <Row className="justify-content-center">
           <Col md={10}>
-          <p><center>TESTE 123</center></p>
+          <p><center>Direcionar Mensagens do Outlook</center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container" style={{ position: 'relative', overflow: 'hidden', paddingTop: '56.25%' }}>
-                <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/EIIiIpbVGlY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
             </BootstrapCard>
           </Col>
