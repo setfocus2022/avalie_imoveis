ee8453ce2f55bf6813bd757c8370f9af49d308f0
qualityseg <analytics.qualityseg@gmail.com>
2023-06-27 09:59:01 -0300

    Update BaseConhecimento.js

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -8,15 +8,27 @@ const BaseConhecimento = () => {
       <Container fluid>        
         <Row>
           <Col>
+          <p><center>TESTE 123</center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container">
-              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/XmJXVXrYQyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>/center>
+              </div>
+            </BootstrapCard>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <p><center>TESTE 123</center></p>
+            <BootstrapCard className="card-stats">
+              <div className="iframe-container">
+              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
               </div>
             </BootstrapCard>
           </Col>
         </Row>
       </Container>
     </>
+    
   );
 };
 
