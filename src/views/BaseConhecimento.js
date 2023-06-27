556a8baeb9b5bd6d04a2fd2baa9da44ae981a6d6
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-27 10:16:24 -0300

    subir

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -8,18 +8,17 @@ const BaseConhecimento = () => {
       <Container fluid>        
         <Row>
           <Col>
-          <p><center>TESTE 123</center></p>
+          <p><center>Video dos nossos cursos EAD para comparitlhar </center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container">
-              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>/center>
+              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
               </div>
             </BootstrapCard>
           </Col>
         </Row>
         <Row>
           <Col>
-          <p><center>Use @menções em seus E-mails #qsdicas
-</center></p>
+          <p><center>Use @menções em seus E-mails #qsdicas</center></p>
             <BootstrapCard className="card-stats">
               <div className="iframe-container">
               <center><iframe width="560" height="315" src="https://www.youtube.com/watch?v=zgv2hH-2rHo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
@@ -27,6 +26,18 @@ const BaseConhecimento = () => {
             </BootstrapCard>
           </Col>
         </Row>
+
+        <Row>
+          <Col>
+          <p><center>Direcionar e-mails para Pastas Personalizadas no Outlook #qsdicas</center></p>
+            <BootstrapCard className="card-stats">
+              <div className="iframe-container">
+              <center><iframe width="560" height="315" src="https://youtu.be/EIIiIpbVGlY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+              </div>
+            </BootstrapCard>
+          </Col>
+        </Row>
+
       </Container>
     </>
     
