fd48363f13042c42a6dddf9c96025bfbedf92572
qualityseg <analytics.qualityseg@gmail.com>
2023-06-27 09:03:44 -0300

    centralizando

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -10,7 +10,7 @@ const BaseConhecimento = () => {
           <Col>
             <BootstrapCard className="card-stats">
               <div className="iframe-container">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/XmJXVXrYQyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <center><iframe width="560" height="315" src="https://www.youtube.com/embed/XmJXVXrYQyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
               </div>
             </BootstrapCard>
           </Col>
