32c6c3f8bc4a0b6f274f3814370c4ec0d3c5135d
qualityseg <analytics.qualityseg@gmail.com>
2023-06-27 11:05:52 -0300

    Update BaseConhecimento.js

--- src/views/BaseConhecimento.js
+++ src/views/BaseConhecimento.js
@@ -16,6 +16,7 @@ const BaseConhecimento = () => {
             </BootstrapCard>
           </Col>
         </Row>
+
         <Row className="justify-content-center">
           <Col md={10}>
           <p><center>TESTE 123</center></p>
@@ -26,6 +27,18 @@ const BaseConhecimento = () => {
             </BootstrapCard>
           </Col>
         </Row>
+
+        <Row className="justify-content-center">
+          <Col md={10}>
+          <p><center>TESTE 123</center></p>
+            <BootstrapCard className="card-stats">
+              <div className="iframe-container" style={{ position: 'relative', overflow: 'hidden', paddingTop: '56.25%' }}>
+                <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/QCB01i2ze-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              </div>
+            </BootstrapCard>
+          </Col>
+        </Row>
+        
       </Container>
     </>
     
