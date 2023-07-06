a5f6d6c1c1eafc6e859e32457f6e9bfcc989b000
qualityseg <analytics.qualityseg@gmail.com>
2023-07-06 10:57:07 -0300

    Update Dashboard.js

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -43,18 +43,6 @@ const Dashboard = () => {
           </div>
         );
         break;
-      case 'realizarOS':
-        setContent(
-          <iframe
-            title="OS 20"
-            width="900"
-            height="536"
-            src="https://forms.zohopublic.com/juliana15/form/ComercialQualitySegEngenhariadeSeguranaeMedicinado/formperma/iKxe7-5DD6rWrFM_duryGABo0x1oPTqqlnpafECN11w"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        );
-        break;
       case 'eSocial':
         setContent(
           <iframe
@@ -129,25 +117,22 @@ const Dashboard = () => {
       <Container fluid>
         <Row>
         <Col lg="2" sm="2" className="text-center mx-auto">
-          <BootstrapCard 
-            className="card-stats card-btn"
-            onClick={handleCardClick('realizarOS')}
-          >
-            <BootstrapCard.Body>
-              <div className="d-flex align-items-center justify-content-center" style={{ marginBottom: '15px' }}>
-                <img
-                  src="https://imgur.com/AtpGO1b.png"
-                  alt="Ícone"
-                  style={{ width: '50px', height: '50px' }}
-                />
-                <BootstrapCard.Title as="h4" style={{ fontSize: '15px', marginLeft: '10px' }}>
-                  <a href="https://forms.zohopublic.com/juliana15/form/ComercialQualitySegEngenhariadeSeguranaeMedicinado/formperma/iKxe7-5DD6rWrFM_duryGABo0x1oPTqqlnpafECN11w" target="_blank">
+          <a href="https://forms.zohopublic.com/juliana15/form/ComercialQualitySegEngenhariadeSeguranaeMedicinado/formperma/iKxe7-5DD6rWrFM_duryGABo0x1oPTqqlnpafECN11w" target="_blank">
+            <BootstrapCard className="card-stats card-btn">
+              <BootstrapCard.Body>
+                <div className="d-flex align-items-center justify-content-center" style={{ marginBottom: '15px' }}>
+                  <img
+                    src="https://imgur.com/AtpGO1b.png"
+                    alt="Ícone"
+                    style={{ width: '50px', height: '50px' }}
+                  />
+                  <BootstrapCard.Title as="h4" style={{ fontSize: '15px', marginLeft: '10px' }}>
                     Realizar OS
-                  </a>
-                </BootstrapCard.Title>
-              </div>
-            </BootstrapCard.Body>
-          </BootstrapCard>
+                  </BootstrapCard.Title>
+                </div>
+              </BootstrapCard.Body>
+            </BootstrapCard>
+          </a>
         </Col>
 
         <Col lg="2" sm="2" className="text-center mx-auto">
