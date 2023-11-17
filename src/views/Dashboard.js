eae600b639eb47d7a4a1089721c321b38365120e
setfocusbrasil <128048171+qualityseg@users.noreply.github.com>
2023-11-17 15:43:26 -0300

    powerbi

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -55,6 +55,17 @@ const Dashboard = () => {
         setContent(
           <div>
             <center>
+
+              <iframe
+                title="Graficos"
+                width="1000"
+                height="635"
+               
+                src="https://app.powerbi.com/view?r=eyJrIjoiNGRjOWQ3OTItODM3Ni00YzEwLWIwMmEtZmM1NDA3NmVmZmYwIiwidCI6ImMxNTk2NDVkLTM3ZjUtNDg5Ny1hNTQ5LTNhZDIzMDkyZTdjNyJ9"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+
               <iframe
                 title="Graficos"
                 width="1000"
