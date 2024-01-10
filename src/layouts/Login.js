e768ff9c77c5375e3030f77739856d650aaded75
setfocusbrasil <128048171+qualityseg@users.noreply.github.com>
2024-01-10 14:05:20 -0300

    graficos

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -37,7 +37,7 @@ const Login = (props) => {
         
         console.log('Role after login:', localStorage.getItem('role')); // Log the role after login
   
-        props.history.push('/admin/Catalogo');
+        props.history.push('/admin/Graficos');
       } else {
         alert('Login falhou');
       }
